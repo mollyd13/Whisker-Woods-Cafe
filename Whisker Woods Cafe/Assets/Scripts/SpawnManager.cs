9663b43625e849f7e62ed196c8edca0feef45544
Molly <mjdaniel@smith.edu>
2024-12-27 20:08:32 -0500

    Make drop prefab and spawn drops.

--- Whisker Woods Cafe/Assets/Scripts/SpawnManager.cs
+++ Whisker Woods Cafe/Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
+    public GameObject drop;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("Spawn", 0, 1);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Spawn() {
+        Instantiate(drop, new Vector3(Random.Range(-12, 12), gameObject.transform.position.y, 0), gameObject.transform.rotation, gameObject.transform);
     }
 }
