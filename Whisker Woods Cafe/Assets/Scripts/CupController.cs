76ae67e47f6d283a720a6761b79cb12fe8a46e85
Molly <mjdaniel@smith.edu>
2024-12-27 20:46:54 -0500

    Add drop sprites and make screen size adjustable.

--- Whisker Woods Cafe/Assets/Scripts/CupController.cs
+++ Whisker Woods Cafe/Assets/Scripts/CupController.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class CupController : MonoBehaviour
 {
     public int speed;
+    Vector3 leftScreen;
+    Vector3 rightScreen;
+    Camera mainCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speed = 35;
+        //get left and right bounds of the viewport
+        mainCamera = Camera.main;
+        leftScreen = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        rightScreen = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane));
+
+        //initialize speed with hardcoded value
+        speed = 25;
     }
 
     // Update is called once per frame
@@ -21,7 +30,7 @@ public class CupController : MonoBehaviour
         //find new position
         Vector3 newPosition = transform.localPosition + Vector3.right * Time.deltaTime * input * speed;
         //clamp position to ensure it stays in bounds
-        newPosition.x = Mathf.Clamp(newPosition.x, -15.7f, 15.7f);
+        newPosition.x = Mathf.Clamp(newPosition.x, leftScreen.x+.5f, rightScreen.x-.5f);
         //set position to equal new calculated and clamped position
         transform.localPosition = newPosition;
     }
--- Whisker Woods Cafe/Assets/Scripts/SpawnManager.cs
+++ Whisker Woods Cafe/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,21 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject drop;
+    Camera mainCamera;
+    Vector3 leftScreen;
+    Vector3 rightScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("Spawn", 0, 1);
+        //get left and right bounds of the viewport
+        mainCamera = Camera.main;
+        leftScreen = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        rightScreen = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane));
+        InvokeRepeating("Spawn", 0, 2);
     }
 
     // Update is called once per frame
     void Spawn() {
-        Instantiate(drop, new Vector3(Random.Range(-12, 12), gameObject.transform.position.y, 0), gameObject.transform.rotation, gameObject.transform);
+        Instantiate(drop, new Vector3(Random.Range(leftScreen.x+.5f, rightScreen.x-.5f), gameObject.transform.position.y, 0), gameObject.transform.rotation, gameObject.transform);
     }
 }
