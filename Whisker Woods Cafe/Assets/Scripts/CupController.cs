d78f8b2cbc491d697f7e466aa79c65a77c57bcc8
Molly <mjdaniel@smith.edu>
2025-01-11 15:41:24 -0500

    Create barebones behind the counter scene.

--- Whisker Woods Cafe/Assets/Scripts/CupController.cs
+++ Whisker Woods Cafe/Assets/Scripts/CupController.cs
@@ -39,7 +39,6 @@ public class CupController : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D collider){
-        Debug.Log("collided");
         scoreManager.increaseScore(collider.gameObject.tag);
         Destroy(collider.gameObject);
     }
